b535c42570a4b48047c76ffce49bf035d0b3d2c5
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2019-06-13 21:27:39 +0000

    setup: crio: crio should be configured at installation
    do not modify crio config the container already do it.
    
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

--- clr-k8s-examples/setup_system_ubuntu.sh
+++ clr-k8s-examples/setup_system_ubuntu.sh
@@ -119,19 +119,6 @@ install_crio(){
 		sudo ln -sf "$runc_path" /usr/bin/runc
 	fi
 
-	crio_config_file="/etc/crio/crio.conf"
-	echo "Set manage_network_ns_lifecycle to true"
-	network_ns_flag="manage_network_ns_lifecycle"
-	sudo sed -i "/\[crio.runtime\]/a$network_ns_flag = true" "$crio_config_file"
-	sudo sed -i 's/manage_network_ns_lifecycle = false/#manage_network_ns_lifecycle = false/' "$crio_config_file"
-
-	echo "Add docker.io registry to pull images"
-	# Matches cri-o 1.10 file format
-	sudo sed -i 's/^registries = \[/registries = \[ "docker.io"/' "$crio_config_file"
-	# Matches cri-o 1.12 file format
-	sudo sed -i 's/^#registries = \[/registries = \[ "docker.io" \] /' "$crio_config_file"
-
-
 	sudo systemctl daemon-reload
 	sudo systemctl restart crio
 	sudo systemctl status crio
